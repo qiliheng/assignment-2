04540a96c1358426af66b513ae221512e4bedd34
www-data <www-data@s5294121.elf.ict.griffith.edu.au>
2024-08-29 10:22:51 +1000

    update

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   standalone: true,
   imports: [],
   templateUrl: './account.component.html',
-  styleUrl: './account.component.css'
+  styleUrls: ['./account.component.css']
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  userId: string | null = '';
+  userName: string | null = '';
+  userBirthdate: string | null = '';
+  userAge: string | null = '';
 
+  ngOnInit() {
+    if (typeof window !== 'undefined' && typeof sessionStorage !== 'undefined') {
+      this.userId = sessionStorage.getItem('userid');
+      this.userName = sessionStorage.getItem('username');
+      this.userBirthdate = sessionStorage.getItem('userbirthdate');
+      this.userAge = sessionStorage.getItem('userage');
+    } else {
+      console.error('sessionStorage is not available');
+    }
+  }
 }
