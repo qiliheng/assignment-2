ca2232d3700b25cbdc75596b8b791e5cc29e8074
www-data <www-data@s5294121.elf.ict.griffith.edu.au>
2024-10-09 21:17:22 +1000

    update server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
+const PORT = 8888;
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
@@ -10,15 +14,54 @@ const io = require('socket.io')(http, {
 });
 const sockets = require('./sockets.js');
 const server = require('./listen.js');
-//const request = require('request');
 
-//Define port used for the server
-const PORT = 8888;
-//Apply express middleware
+// Apply middleware
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//setup Socket
+// Serve static files from the data directory
+app.use('/server/data', express.static(path.join(__dirname, 'data')));
+
+// Login routes
+app.post('/login', require('./router/postLogin'));
+app.post('/joinChannel', require('./router/postJoinChannel'));
+app.post('/CreateGroup', require('./router/postCreateGroup'));
+app.post('/createChannel', require('./router/postCreateChannel'));
+app.post('/deleteGroup', require('./router/postDeleteGroup'));
+app.post('/deleteChannel', require('./router/postDleteChannel'));
+app.post('/promoteToGroupAdmin', require('./router/postPromoteToGroupAdmin'));
+app.post('/promoteToSuperAdmin', require('./router/postPromoteToSuperAdmin'));
+app.post('/removeUser', require('./router/postRemoveUser'));
+app.post('/joinGroup', require('./router/postJoinGroup'));
+app.post('/createUser', require('./router/postCreateUser'));
+
+// Delete user route
+app.post('/deleteUser', (req, res) => {
+    const { username } = req.body;
+    const usersPath = path.join(__dirname, 'data', 'users.json');
+    
+    fs.readFile(usersPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Server error reading users.json');
+        }
+        let users = JSON.parse(data);
+        const updatedUsers = users.filter(user => user.username !== username);
+
+        fs.writeFile(usersPath, JSON.stringify(updatedUsers, null, 2), 'utf8', (err) => {
+            if (err) {
+                return res.status(500).send('Server error writing users.json');
+            }
+            res.send({ success: true, message: `User ${username} deleted successfully` });
+        });
+    });
+});
+
+// Setup socket connections
 sockets.connect(io, PORT);
 
-//Start server listening for requests.
-server.listen(http, PORT);
+// Start server listening for requests
+http.listen(PORT, () => {
+    console.log('Server listening on: ' + PORT);
+    server.listen(http, PORT);
+});
